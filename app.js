4923597f69c4e678409ed895d42e774ff2c18369
Sambhaji Mane <mane.sambhaji9@gmail.com>
2021-12-12 22:05:16 +0530

    First initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = new express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.send("Hello Johannesburg");
+});
+
+app.listen(port, function() {
+    console.log(`Server started at port ${port}`);
+});
