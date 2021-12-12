51b4d41ff1752adbca8b3a33bf4fef20a35a1e03
Sambhaji Mane <mane.sambhaji9@gmail.com>
2021-12-12 22:35:25 +0530

    Added one dummy data.json file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = new express();
+var fileSys = require('fs');
 
 var port = process.env.PORT || 3000;
 
@@ -9,6 +10,13 @@ app.get('/', function (req, res) {
 
 app.get('/about', function (req, res) {
     res.send("This is about page");
+});
+
+app.get('/list', function (req, res) {
+    fileSys.readFile('./data.json', function(err, data) {
+        console.log(JSON.parse(data));
+        res.status(200).send(JSON.parse(data));
+    });
 });
 
 app.listen(port, function() {
