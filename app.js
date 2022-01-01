b7f6901db1ee05131e1eb426ce5acca958382769
Sambhaji Mane <mane.sambhaji9@gmail.com>
2022-01-01 11:42:08 +0530

    Added connection to mongodb database on atlas

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = new express();
 var fileSys = require('fs');
+var MongoClient = require('mongodb').MongoClient;
+var db = require('./db');
 
 var port = process.env.PORT || 3000;
 
@@ -9,16 +11,26 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-    res.send("This is about page");
+    MongoClient.connect(db.connection.url, function (err, db) {
+        if (err) throw err;
+
+        var dbo = db.db("johannesburg");
+        dbo.collection("todolist").find().toArray((err, result) => {
+            if (err) throw err;
+
+            res.status(200).send(result);
+            db.close();
+        });
+    });
 });
 
 app.get('/list', function (req, res) {
-    fileSys.readFile('./data.json', function(err, data) {
+    fileSys.readFile('./data.json', function (err, data) {
         console.log(JSON.parse(data));
         res.status(200).send(JSON.parse(data));
     });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log(`Server started at port ${port}`);
 });
---
+++ db.js
@@ -0,0 +1,3 @@
+module.exports.connection = {
+    url: "mongodb+srv://m001-student:admin@cluster0.lqbum.mongodb.net/"
+};
